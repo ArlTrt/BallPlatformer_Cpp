98ea1227bfd2ab4edd516a236bf654c905d24057
Arl44 <117769424+ArlTrt@users.noreply.github.com>
2025-03-26 00:53:14 +0100

    Classes Setup
    Beam, Connector, Anchor

---
+++ BallPlatformer_Cpp/Source/BallPlatformer_Cpp/Anchor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Connector.h"
+#include "Anchor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALLPLATFORMER_CPP_API AAnchor : public AConnector
+{
+	GENERATED_BODY()
+	
+};
